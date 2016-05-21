6601215cae8a186a570d8f30dc97e62a0068aa5b
ess_ess <sravans2011@gmail.com>
2016-05-21 22:13:05 +0530

    fixed resource file issues

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -2,7 +2,7 @@
 
 (function ($) {
     $(".main-slider").vegas({
-        slides: [{ src: "/assets/page-1_slide01.jpg" }, { src: "/assets/page-1_slide02.jpg" }, { src: "/assets/page-1_slide03.jpg" }],
+        slides: [{ src: "/church-template/assets/page-1_slide01.jpg" }, { src: "/church-template/assets/page-1_slide02.jpg" }, { src: "/church-template/assets/page-1_slide03.jpg" }],
         timer: false
     });
 })(jQuery);
