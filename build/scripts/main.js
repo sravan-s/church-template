6d635560b2cd4a5f03a6a40e5e459d089c3dae27
ess_ess <sravans2011@gmail.com>
2016-05-21 20:39:23 +0530

    intial commit

---
+++ build/scripts/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+(function ($) {
+    $(".main-slider").vegas({
+        slides: [{ src: "/assets/page-1_slide01.jpg" }, { src: "/assets/page-1_slide02.jpg" }, { src: "/assets/page-1_slide03.jpg" }],
+        timer: false
+    });
+})(jQuery);
